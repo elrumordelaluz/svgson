45e7f5ff8c08433473ec693654b932ca2ad7c78c
Lionel T <elrumordelaluz@hotmail.com>
2015-12-30 14:54:03 +0100

    Add Tape tests and improve module exports

--- index.js
+++ index.js
@@ -19,105 +19,88 @@ var camelCase = function (prop) {
   });
 };
 
-var toObject = (function (input){
-  'use strict';
-
-  var initialize = function(options) {
-    var config = {
-      input: input,
-      json: false,
-      svgo: false
-    };
-    
-    for(var prop in options) {
-      if(options.hasOwnProperty(prop)){
-        config[prop] = options[prop];
-      }
+module.exports = function(input, options) {
+  var config = {
+    json: false,
+    svgo: false
+  };
+  
+  for(var prop in options) {
+    if(options.hasOwnProperty(prop)){
+      config[prop] = options[prop];
     }
+  }
 
-    var data;
+  var data;
 
-    var parse = function(input) {
-      var doc = new DOMParser().parseFromString(input);
-      return doc.documentElement ? doc : undefined;
-    };
-
-    var generate = function(source) {
-      var obj = {}; 
-      if (source.nodeType === 1) {
-        obj.name = source.nodeName;
-
-        if (source.attributes.length > 0) {
-          obj.attrs = {};
-          [].slice.call(source.attributes).forEach(function(item) {
-            obj.attrs[camelCase(item.name)] = item.value;
-          });
-        }
-
-      } else if (source.nodeType == 3 && /\S/.test(source.nodeValue)) {
-        obj.name = 'text';
-        obj.content = source.nodeValue;
-      }
-
-      if (source.hasChildNodes()) {
-        var elements = [];
-        for (var i = 0; i < source.childNodes.length; i++) {
-          var item = source.childNodes.item(i);
-          if (item.nodeType == 1 || (item.nodeType == 3 && /\S/.test(item.nodeValue))) {
-            var nodeName = item.nodeName;
-            elements.push(generate(item));
-          }
-        };
-        obj['childs'] = elements;
-      }
-
-      return obj;
-    };
-
-    var optimize = function (input) {
-      svgo.optimize(input, function(result) {
-          return result.data;
-      });
-    }
-
-    // Do..
-    if (exist(config.input)) {
-      data = fs.readFile(config.input, 'utf8', function(e, d){
-        var r;
-        if (parse(d)) {
-          if (config.svgo) {
-            svgo.optimize(d, function(result) {
-              r = parse(result.data);
-            });  
-          } else {
-            r = parse(d);
-          }
-        }
-        return r ? console.log(JSON.stringify(generate(r))) : false;
-      })
-    } else {
-      data = config.input;
-      var r;
-      if (parse(data)) {
-        if (config.svgo) {
-          svgo.optimize(data, function(result) {
-            r = parse(result.data);
-          });  
-        } else {
-          r = parse(data);
-        }
-      }
-      return r ? console.log(JSON.stringify(generate(r))) : false;
-    }
-
+  var parse = function(input) {
+    var doc = new DOMParser().parseFromString(input);
+    return doc.documentElement ? doc : undefined;
   };
 
-  return {
-    init: initialize
+  var generate = function(source) {
+    var obj = {}; 
+    if (source.nodeType === 1) {
+      obj.name = source.nodeName;
+
+      if (source.attributes.length > 0) {
+        obj.attrs = {};
+        [].slice.call(source.attributes).forEach(function(item) {
+          obj.attrs[camelCase(item.name)] = item.value;
+        });
+      }
+
+    } else if (source.nodeType == 3 && /\S/.test(source.nodeValue)) {
+      obj.name = 'text';
+      obj.content = source.nodeValue;
+    }
+
+    if (source.hasChildNodes()) {
+      var elements = [];
+      for (var i = 0; i < source.childNodes.length; i++) {
+        var item = source.childNodes.item(i);
+        if (item.nodeType == 1 || (item.nodeType == 3 && /\S/.test(item.nodeValue))) {
+          var nodeName = item.nodeName;
+          elements.push(generate(item));
+        }
+      };
+      obj['childs'] = elements;
+    }
+
+    return config.json ? JSON.stringify(obj, null, 2) : obj;
   };
-})();
 
+  var optimize = function (input) {
+    svgo.optimize(input, function(result) {
+        return result.data;
+    });
+  }
 
-module.exports = function(config) {
-  return toObject.init(config || {});
+  var r;
+  if (exist(input)) {
+    data = fs.readFileSync(input, 'utf8')
+    if (parse(data)) {
+      if (config.svgo) {
+        svgo.optimize(data, function(result) {
+          r = parse(result.data);
+        });  
+      } else {
+        r = parse(data);
+      }
+    }
+    return r ? generate(r) : false;
+  } else {
+    data = input;
+    if (parse(data)) {
+      if (config.svgo) {
+        svgo.optimize(data, function(result) {
+          r = parse(result.data);
+        });  
+      } else {
+        r = parse(data);
+      }
+    }
+    return r ? generate(r) : false;
+  }
+  
 };
