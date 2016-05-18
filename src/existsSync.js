70fc30daec08befb184548f976309f0421ec5035
Lionel T <elrumordelaluz@hotmail.com>
2016-05-18 11:04:16 +0200

    Update new code base

--- src/existsSync.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-module.exports = fs.existsSync || function existsSync(filePath){
-  try{
-    fs.statSync(filePath);
-  }catch(err){
-    if(err.code == 'ENOENT') return false;
-  }
-  return true;
-};
