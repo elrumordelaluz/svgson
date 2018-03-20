cdcf23d49cdc2bdb9eac152dc748a10043143c1b
Jimmy Loughran <jimmy.loughran1@ie.ibm.com>
2018-03-20 18:17:01 +0000

    Wrap stdouts in a isTTY check

--- bin/svgson.js
+++ bin/svgson.js
@@ -8,7 +8,7 @@ const path    = require('path');
 const Promise = require('promise');
 const chalk   = require('chalk');
 const svgson  = require('../lib/svgson');
-const version = require('../package.json').version
+const version = require('../package.json').version;
 const list = (val) => val.split(',');
 
 program
@@ -120,9 +120,11 @@ const processFile = (file) => {
   const fileName = path.basename(file, fileExt);
   return new Promise((resolve,reject) => {
     return fs.readFile(filePath, 'utf8').then(data => {
-      process.stdout.cursorTo(0);
-      process.stdout.clearLine();
-      process.stdout.write(file);
+      if (require('tty').isatty(1)) {
+        process.stdout.cursorTo(0);
+        process.stdout.clearLine();
+        process.stdout.write(file);
+      }
       svgson(data, applyExtras(fileName), resolve);
     });
   });
@@ -133,8 +135,10 @@ const toJSON = (obj, pretty) => {
 };
 
 const printFile = (obj) => {
-  process.stdout.clearLine();
-  process.stdout.cursorTo(0);
+  if (require('tty').isatty(1)) {
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+  }
   console.log(`--- ${chalk.yellow('Transforming into')}${program.pretty ? chalk.gray(' Prettyfied') : ''} ${chalk.yellow('JSON notation')}`)
   return toJSON(obj, program.pretty);
 };
