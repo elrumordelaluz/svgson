a6b61e5f0d83b0015c5d6792201af34e38b146d5
Mihail Buyskih <mihail@mixbook.com>
2020-05-01 15:31:20 +0300

    Implement parseSync and options

--- types.d.ts
+++ types.d.ts
@@ -1,13 +1,27 @@
 declare module "svgson" {
-  export interface ISvgObject {
+  export interface INode {
     name: string,
     type: string,
     value: string,
     attributes: Record<string, string>,
-    children: ISvgObject[]
+    children: INode[]
   }
 
-  function parse(content: string): Promise<ISvgObject>;
+  interface IParseOptions {
+    transformNode?: (node: INode) => INode,
+    camelcase?: boolean
+  }
 
-  function stringify(object: ISvgObject): string;
+  type TEscape = (text?: string) => string;
+
+  interface IStringifyOptions {
+    transformAttr?: (key: string, value: string, escape: TEscape) => string;
+    selfClose?: boolean;
+  }
+
+  function parse(input: string, options?: IParseOptions): Promise<INode>;
+
+  function parseSync(input: string, options?: IParseOptions): INode;
+
+  function stringify(ast: INode, options?: IStringifyOptions): string;
 }
